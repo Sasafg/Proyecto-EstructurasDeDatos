1b0ee0dd0916bf9125d4ceef63af62a68f7baa7c
Sasafg <saryfg@gmail.com>
2022-11-22 10:18:12 -0600

    act
    creacion de frames

---
+++ ProyectoEstructuras/src/main/java/com/example/demo/ProyectoDeEstructurasApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoDeEstructurasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoDeEstructurasApplication.class, args);
+	}
+
+}
